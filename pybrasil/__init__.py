70a25a51842776341b3bd1aa0e0bb79654c6d048
Renato Lima <renato.lima@akretion.com.br>
2019-02-25 21:32:25 -0300

    fix

--- pybrasil/__init__.py
+++ pybrasil/__init__.py
@@ -6,17 +6,3 @@ from __future__ import (division, print_function, unicode_literals,
 
 from . import (base, codigo_barras, data, febraban, feriado, ibge, inscricao, ncm, produto, telefone,
                template, valor)
-from .python_pt_BR import python_pt_BR
-
-import sys
-import locale
-
-if sys.version_info.major == 2:
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
-    locale.setlocale(locale.LC_ALL, b'pt_BR.UTF-8')
-    locale.setlocale(locale.LC_COLLATE, b'pt_BR.UTF-8')
-
-else:
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-    locale.setlocale(locale.LC_COLLATE, 'pt_BR.UTF-8')
